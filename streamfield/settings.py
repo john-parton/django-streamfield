e33c4e15824a4c7ca1a3c0be1aa6111d4a9b32f6
John Parton <john.parton@plushrugs.com>
2020-09-11 12:38:36 -0500

    Comment

--- streamfield/settings.py
+++ streamfield/settings.py
@@ -5,3 +5,6 @@ BLOCK_OPTIONS = getattr(settings, "STREAMFIELD_BLOCK_OPTIONS", {})
 SHOW_ADMIN_HELP_TEXT = getattr(settings, "STREAMFIELD_SHOW_ADMIN_HELP_TEXT", True)
 # Consider flipping this one
 DELETE_BLOCKS_FROM_DB = getattr(settings, "STREAMFIELD_DELETE_BLOCKS_FROM_DB", True)
+
+# TODO Create a management command to cleanup unused blocks... not sure how that would work
+# How to follow relationships backward?
